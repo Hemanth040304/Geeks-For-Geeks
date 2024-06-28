9a9b7864d819eb0f49d03ba12ff0b4711613f047
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-06-28 12:12:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a number represented as linked list/add-1-to-a-number-represented-as-linked-list.cpp
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data%10; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+// } Driver Code Ends
+//User function template for C++
+
+/* 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution {
+private:
+    int add(Node *head) {
+        if (head == nullptr) return 1;
+        
+        int sum = head -> data + add(head -> next);
+        head -> data = sum % 10;
+    
+        return sum / 10;
+    }
+    
+public:
+    Node* addOne(Node *head) {
+        int carry = add(head);
+        
+        if (carry == 1) {
+            Node* newHead = new Node(1);
+            newHead -> next = head;
+            return newHead;
+        }
+        
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        
+        Node* head = new Node( s[0]-'0' );
+        Node* tail = head;
+        for(int i=1; i<s.size(); i++)
+        {
+            tail->next = new Node( s[i]-'0' );
+            tail = tail->next;
+        }
+        Solution ob;
+        head = ob.addOne(head);
+        printList(head); 
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
