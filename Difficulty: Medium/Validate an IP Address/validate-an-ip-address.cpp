9f05bbb435941981b6684f46d7d00ebc2b7163f0
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-08-06 09:48:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Validate an IP Address/validate-an-ip-address.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/* The function returns 1 if
+IP string is valid else return 0
+You are required to complete this method */
+class Solution {
+  public:
+    int isValid(string str) {
+        // code here
+        int n = 0;
+        for (char c : str) {
+            if (c != '.') n = n * 10 + c - '0';
+            else if (c == '.') {
+                if (n < 0 or n > 255) return false;
+                else n = 0;
+            }
+        }
+        
+        if (n < 0 or n > 255) return false;
+        
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        // if (s.size() == 3) {
+        //     cout << "false" << endl;
+        //     return 0;
+        // }
+        Solution ob;
+        bool f = ob.isValid(s);
+        if (f)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
