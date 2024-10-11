5cbccdbebeda50f2d5736ddeabaffdf809050784
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-10-11 15:26:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reorganize The Array/reorganize-the-array.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> rearrange(const vector<int>& arr) {
+        // Code here
+        vector<int> ans;
+        unordered_map<int, int> mp;
+        int n = arr.size();
+        
+        for (auto it : arr) mp[it]++;
+        
+        for (int i = 0; i < n; i++) {
+            if (mp[i]) ans.emplace_back(i);
+            else ans.emplace_back(-1);
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        vector<int> arr;
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution solution;
+        vector<int> ans = solution.rearrange(arr);
+
+        for (int i = 0; i < ans.size(); i++)
+            cout << ans[i] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
