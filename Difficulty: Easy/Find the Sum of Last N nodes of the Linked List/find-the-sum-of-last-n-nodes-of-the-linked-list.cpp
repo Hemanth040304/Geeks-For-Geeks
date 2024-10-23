4b037f67298d4bb1ae82ec42e8d3e686988fda0d
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-10-23 10:46:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find the Sum of Last N nodes of the Linked List/find-the-sum-of-last-n-nodes-of-the-linked-list.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    /*Structure of the node of the linled list is as
+
+    struct Node {
+        int data;
+        struct Node* next;
+
+        Node(int x){
+            data = x;
+            next = NULL;
+        }
+    };
+    */
+    // your task is to complete this function
+    // function should return sum of last n nodes
+    int sumOfLastN_Nodes(struct Node* head, int n) {
+        // Code here
+        vector<int> pre;
+        while (head) {
+            if (pre.empty()) pre.emplace_back(head -> data);
+            else pre.emplace_back(head -> data + pre.back());
+            head = head -> next;
+        }
+        
+        if (pre.size() == n) return pre.back();
+        
+        return pre.back() - pre[pre.size() - n - 1];
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n;
+        cin >> n;
+        cin.ignore();
+        struct Node *head = new Node(arr[0]);
+        struct Node *tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+        Solution ob;
+        cout << ob.sumOfLastN_Nodes(head, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
